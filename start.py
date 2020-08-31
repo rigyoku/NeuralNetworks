b239307bdef1f8263cef2435fce2889d9a9b0dda
MrLi <942138429@qq.com>
2020-08-31 21:51:55 +0800

    2020/8/31
    采样方式减少数据,修正数据拼接

--- start.py
+++ start.py
@@ -3,19 +3,25 @@ import imgProcessing
 import numpy as np
 import matplotlib.pyplot as mpt
 
+# 取得数据
 data = imgProcessing.getData()
 input = data.get('input')
 testInput = data.get('testInput')
 label = data.get('label')
 testLabel = data.get('testLabel')
 
-net = sgd.SgdNet(input.shape[1], label.shape[1], 5)
+# 对总数据进行1000个抽样
+sampling = np.random.choice(input.shape[0], 10)
+input = input[sampling]
+label = label[sampling]
+
+net = sgd.SgdNet(input.shape[1], label.shape[1], True, 28)
 loss = []
 correctRate = []
 testRate = []
 for index in range(0, 3):
-    l, c = net.batchLearn(input, label, 10000, 0.01, 10)
-    loss.append(l)
+    l, c = net.batchLearn(input, label, 5, 0.01, 10)
+    loss.extend(l)
     correctRate.append(c)
     testRate.append(net.correctRate(net.output(testInput), testLabel))
 
